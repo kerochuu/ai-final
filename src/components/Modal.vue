1f45ab438d2d838f7a411c8c6ed328db4f91c70c
Joowon Lee <marulover7@gmail.com>
2019-08-02 14:45:13 +0900

    login modify

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -152,10 +152,10 @@ export default {
 
 .modal-container {
   width: 400px;
-  height: 600px;
+  height: 500px;
   padding: 20px 30px;
   background-color: #fff;
-  border-radius: 2px;
+  border-radius: 10px;
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
   transition: all 0.3s ease;
   font-family: Helvetica, Arial, sans-serif;
