e52bc6f023b05495f27c3212026bf0254fc4be73
Joowon Lee <marulover7@gmail.com>
2019-08-06 17:51:57 +0900

    backgnd 사진 설정, slider 생성-수정 많이하기

--- src/components/ImgBanner.vue
+++ src/components/ImgBanner.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
     <v-img id="bannerImg" :src="imgSrc" aspect-ratio="1.7">
-      <v-layout align-center justify-center row fill-height>
+      <!-- <v-layout align-center justify-center row fill-height>
         <v-flex text-xs-center>
           <span class="text-shadow display-2 font-weight-light">
             <slot name="text" />
           </span>
         </v-flex>
-      </v-layout>
+      </v-layout> -->
     </v-img>
   </div>
 </template>
@@ -26,11 +26,20 @@ export default {
 </script>
 <style>
 #bannerImg {
- /* background-image: linear-gradient(to bottom right, #e39b82, #fef3cb);*/
+  background-image:url("../assets/backgndImg/wallpaper.png");
+  background-size: cover;
+  background-position: bottom;
   height: 100vh;
+  width: 100%;
+  max-width:100%;
+ /* background-image: linear-gradient(to bottom right, #e39b82, #fef3cb);*/
   }
 .text-shadow {
   text-shadow: 0 0 15px rgb(255, 255, 255);
 }
-
+@media screen and (max-width:460px){
+ #bannerImg{
+   height: 100vh;
+ }
+};
 </style>
--- src/components/PortfolioList.vue
+++ src/components/PortfolioList.vue
@@ -1,58 +1,87 @@
 <template>
-  <v-layout mt-5 wrap>
-    <v-flex xs12 sm6 md3
-    v-for="i in portfolios.length > limits ? limits : portfolios.length" >
-      <Portfolio class="ma-3"
+  <md-card>
+    <md-card-media>
+      <!-- swiper -->
+      <swiper :options="swiperOption">
+        <swiper-slide v-for="i in range">
+          <Portfolio
+              class="ma-5"
               :date="portfolios[i - 1].created_at.toString()"
               :title="portfolios[i - 1].title"
               :body="portfolios[i - 1].body"
               :imgSrc="portfolios[i - 1].img"
-              :pid="portfolios[i-1].pid"
-      ></Portfolio>
-    </v-flex>
-
-    <v-flex xs12 text-xs-center round my-5 v-if="loadMore">
-      <v-btn color="info" dark v-on:click="loadMorePortfolios"><v-icon size="25" class="mr-2">fa-plus</v-icon> 더 보기</v-btn>
-    </v-flex>
-  </v-layout>
+              :pid="portfolios[i-1].pid"/>
+        </swiper-slide>  
+        <div class="swiper-button-prev" slot="button-prev"></div>
+        <div class="swiper-button-next" slot="button-next"></div>
+      </swiper>
+    </md-card-media>
+  </md-card>
 </template>
+
 <script>
-import Portfolio from '@/components/Portfolio'
-import FirebaseService from '@/services/FirebaseService'
-
+import Portfolio from "@/components/Portfolio";
+import FirebaseService from "@/services/FirebaseService";
+import "swiper/dist/css/swiper.css";
+import { swiper, swiperSlide } from "vue-awesome-swiper";
 export default {
-	name: 'PortfoliosList',
-	props: {
-		limits: {type: Number, default: 4},
-    loadMore: {type: Boolean, default: false}
-	},
-	data() {
-		return {
-			portfolios: []
-		}
-	},
-	components: {
-		Portfolio
-	},
-	mounted() {
-		this.getPortfolios()
-	},
-	methods: {
-		async getPortfolios() {
-      this.portfolios = await FirebaseService.getPortfolios()
-		},
-		loadMorePortfolios() {
-      this.limits += 4;
-
-      if(this.limits >= this.portfolios.length)
-        this.loadMore = false;
+  name: "PortfoliosList",
+  props: {
+    // limits: { type: Number, default: 3 },
+    // loadMore: { type: Boolean, default: false }
+  },
+  components: {
+    Portfolio,
+    swiper,
+    swiperSlide
+  },
+  mounted() {
+    this.getPortfolios();
+    console.log(this.range)
+    
+  },
+  methods: {
+    async getPortfolios() {
+      this.portfolios = await FirebaseService.getPortfolios();
+      const length = this.portfolios.length > 15 ? 15 : this.portfolios.length;
+      for (let i = 1; i <= length; i++) {
+        this.range.push(i)
+      }
     }
-	},
-}
+    // loadMorePortfolios() {
+    //   this.limits += 3;
+    //   if (this.limits >= this.portfolios.length) this.loadMore = false;
+    // }
+  },
+  data() {
+    return {
+      portfolios: [],
+      range: [],
+      swiperOption: {
+        slidesPerView: 4,
+        spaceBetween: 30,
+        slidesPerGroup: 3,
+        loop: true,
+        loopFillGroupWithBlank: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev"
+        }
+      }
+    };
+  }
+};
 </script>
 <style>
-  .mw-700 {
-    max-width: 700px;
-    margin: auto;
-  }
+.mw-700 {
+  max-width: 700px;
+  margin: auto;
+}
+.swiper-wrapper > div {
+  width : 23.7% !important
+}
 </style>
