e1fcb042fd23d003d158dc8a28d2407bc78306cb
gyungsoo Kim <wlsl4239@naver.com>
2019-08-08 10:28:34 +0900

    Postpage banner 추가

--- src/views/PostPage.vue
+++ src/views/PostPage.vue
@@ -1,6 +1,9 @@
 <template>
-  <div>
+  <div style="margin: 100px 0px;">
     <v-container>
+      <div>
+        <h1 style="text-align:center; font-size:8vh;">Post Page</h1>
+      </div>
       <!-- Post -->
       <v-layout>
         <v-flex sm12>
