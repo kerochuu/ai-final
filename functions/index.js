8c6e8e2d52e3d8c8d36804848670129b984f21ab
Joowon Lee <marulover7@gmail.com>
2019-08-13 17:14:50 +0900

    firebase hositing, postlist 수정

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,108 @@
 const functions = require('firebase-functions');
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.newComment = functions.firestore.document('comment/{documentId}').onCreate((snap, context) => {
+    const newValue = snap.data();
+    const key = 'AAAAMEGt6WY:APA91bGYzHueKCzlCKDxg0OID0i1tWy0e-UsfTRIBygT5cjE2HJFpKfRSfSHtooex7LXZh1d1lTc-eKWg7S_0BZRIFtCH4I3YEmKlEYYpCJK-MRK58xa5uHIp6jARmqVa3R5WJ8YCXj8';
+    var notification = {
+        'title': newValue.writer + '님이 댓글을 작성하셨습니다',
+        'body': 'Title : ' + newValue.text,
+    };
+    let list = []
+    let cnt = 0
+    firestore.collection(POSTS).doc(newValue.portfolioId)
+        .get()
+        .then((doc) => {
+            if (doc.exists) {
+                fcmToken = doc.data().fcmToken;
+                list[cnt++] = doc.data().fcmToken
+                return notify(key, notification, list);
+            } else {
+                return;
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+    firestore.collection(PORTFOLIOS).doc(newValue.portfolioId)
+        .get()
+        .then((doc) => {
+            if (doc.exists) {
+                fcmToken = doc.data().fcmToken;
+                list[cnt++] = doc.data().fcmToken
+                return notify(key, notification, list);
+            } else {
+                return;
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+})
+exports.newPost = functions.firestore.document('posts/{documentId}').onCreate((snap, context) => {
+    const newValue = snap.data();
+    const key = 'AAAAMEGt6WY:APA91bGYzHueKCzlCKDxg0OID0i1tWy0e-UsfTRIBygT5cjE2HJFpKfRSfSHtooex7LXZh1d1lTc-eKWg7S_0BZRIFtCH4I3YEmKlEYYpCJK-MRK58xa5uHIp6jARmqVa3R5WJ8YCXj8';
+    const notification = {
+        'title': newValue.displayName + '님이 Post를 작성하셨습니다',
+        'body': 'Title : ' + newValue.title,
+        'click_action' : 'https://webproject-myblog.firebaseapp.com',
+    };
+    let list = []
+    let cnt = 0
+    const subScriberCollection = firestore.collection(SUBSCRIBER)
+    return subScriberCollection
+        .get()
+        .then((docSnapshots) => {
+            docSnapshots.docs.map((doc) => {
+                let snapData = doc.data()
+                if (newValue.fcmToken !== snapData.token) {
+                    list[cnt++] = snapData.token;
+                }
+                return;
+            })
+            return notify(key, notification, list)
+        })
+});
+exports.newPortfolio = functions.firestore.document('portfolios/{documentId}').onCreate((snap, context) => {
+    const newValue = snap.data();
+    const key = 'AAAAMEGt6WY:APA91bGYzHueKCzlCKDxg0OID0i1tWy0e-UsfTRIBygT5cjE2HJFpKfRSfSHtooex7LXZh1d1lTc-eKWg7S_0BZRIFtCH4I3YEmKlEYYpCJK-MRK58xa5uHIp6jARmqVa3R5WJ8YCXj8';
+    const notification = {
+        'title': newValue.displayName + '님이 Portfolio를 작성하셨습니다',
+        'body': 'Title : ' + newValue.title,
+    };
+    let list = []
+    let cnt = 0
+    const subScriberCollection = firestore.collection(SUBSCRIBER)
+    return subScriberCollection
+        .get()
+        .then((docSnapshots) => {
+            docSnapshots.docs.map((doc) => {
+                let snapData = doc.data()
+                if (newValue.fcmToken !== snapData.token) {
+                    list[cnt++] = snapData.token;
+                }
+                return;
+            })
+            return notify(key, notification, list)
+        })
+});
+function notify(key, notification, list) {
+    axios({
+        url: 'https://fcm.googleapis.com/fcm/send',
+        method: 'POST',
+        headers: {
+            'Authorization': 'key=' + key,
+            'Content-Type': 'application/json'
+        },
+        data: JSON.stringify({
+            'notification': notification,
+            'registration_ids': list
+        })
+    })
+        .then(response => {
+            console.log("성공~")
+            return response
+        })
+        .catch(error => {
+            console.log("실패~")
+            return error
+        })
+}
--- src/views/PostPage.vue
+++ src/views/PostPage.vue
@@ -1,20 +1,35 @@
 <template>
+<div class="postPage">
+  <div>
+    
+  </div>
+</div>
+
   <div style="margin: 100px 0px;">
     <v-container>
       <div>
         <h1 style="text-align:center; font-size:8vh;">Post Page</h1>
       </div>
+
+      <v-flex xs12 text-xs-center>
+        <v-btn
+          v-if="'Anonymous' != isGuest()"
+          class="postWriteBtn"
+          color="success"
+          fab
+          dark
+          small
+          @click="movePostWriter"
+        >
+          <v-icon dark>edit</v-icon>
+        </v-btn>
+      </v-flex>
       <!-- Post -->
       <v-layout>
         <v-flex sm12>
           <PostList :limits="4" :load-more="true"></PostList>
         </v-flex>
       </v-layout>
-
-      <v-btn v-if="'Anonymous' != isGuest()" class="mx-2" style="margin:0%, 5%, 0%, 80%;" color="info" fab dark small @click="movePostWriter">
-                <v-icon dark>edit</v-icon>
-       </v-btn>
-
     </v-container>
   </div>
 </template>
@@ -27,29 +42,50 @@ export default {
   components: {
     PostList
   },
-   methods: {
+  methods: {
     isGuest() {
       return this.authority;
     },
-    movePostWriter(){
-      this.$router.push('/postwrite');
+    movePostWriter() {
+      this.$router.push("/postwrite");
     }
   },
   props: {
     authority: { type: String }
   },
-   data() {
+  data() {
     return {
-      user:{}
+      user: {}
     };
   },
   mounted() {
-   
     FirebaseService.getInfo().then(res => {
       this.user = res;
       alert(user);
     });
-
   }
 };
 </script>
+
+<style scoped>
+.postPage{
+  margin: 100px 20px;
+}
+@media screen and (min-width: 600px){
+  .postPage{
+    margin: 100px 40px;
+  }
+}
+
+@media screen and (min-width: 960px){
+  .postPage{
+    margin: 100px 100px;
+  }
+}
+
+@media screen and (min-width: 1264px){
+  .postPage{
+    margin: 100px 200px; 
+  }
+}
+</style>
